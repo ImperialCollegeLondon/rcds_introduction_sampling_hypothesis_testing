5ea7f380c1744f2b4233d28c572e495b0ee3d6e7
Jesus Urtasun <jurtasun@MRC-11016.local>
2024-03-23 13:00:20 +0000

    Add exercises - chapter2

--- chapter2/exercises/central_limit_theorem.py
+++ chapter2/exercises/central_limit_theorem.py
@@ -8,23 +8,35 @@ import matplotlib.pyplot as plt
 import pdb
 
 # Define parameters
-population_distribution = np.random.randint(1, 7, 1000)  # Uniform distribution from 1 to 6
+population_distribution = np.random.randint(1, 7, 1000)  # Uniform distribution from 1 to 6 # bins = range(1, 8)
+# population_distribution = np.random.binomial(n = 10, p = 0.5, size = 1000)
+# population_distribution = np.random.poisson(lam = 5, size = 1000) # bins = range(-10, 10)
+# population_distribution = np.random.normal(loc = 0, scale = 1, size = 1000) # bins = range(-10, 10)
 
 # Define sample sizes for demonstration
 sample_sizes = [10, 50, 200]
 
+# Plot original population distribution
+plt.hist(population_distribution, bins = range(1, 8), edgecolor = 'black', align = 'left', density = True)
+plt.title('Original Population Distribution')
+plt.xlabel('Value')
+plt.ylabel('Probability')
+plt.show()
+
 # Plot settings
-plt.figure(figsize=(12, 8))
+plt.figure(figsize = (12, 8))
 
 # Plot original population distribution
 plt.subplot(2, 2, 1)
-plt.hist(population_distribution, bins=range(1, 8), edgecolor='black', align='left', density=True)
+plt.hist(population_distribution, bins = range(1, 8), edgecolor = 'black', align = 'left', density = True)
 plt.title('Original Population Distribution')
 plt.xlabel('Value')
 plt.ylabel('Probability')
 
 # Iterate over different sample sizes
 for i, sample_size in enumerate(sample_sizes):
+
+    # Get 
     sample_means = [np.mean(np.random.choice(population_distribution, sample_size)) for _ in range(1000)]
     
     # Plot sample mean distribution
